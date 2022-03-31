7a1459bc705160707eb13c89ccdbe146f5639d0f
Ajay <ajay.gupta1711@gmail.com>
2022-03-31 21:06:09 +0530

    ArrayList and HashSet Interface

---
+++ src/ArrayList/arrayListExample.java
@@ -0,0 +1,24 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class arrayListExample {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("Ajay");
+		a.add("Java");
+		a.add("Java");
+		a.add("Aarav");
+		System.out.println(a);
+		
+		System.out.println(a.indexOf("Ajay"));
+		
+		System.out.println(a.isEmpty());
+		
+		System.out.println(a.size());
+		
+	}
+
+}
---
+++ src/SetInterface/hashSetExample.java
@@ -0,0 +1,15 @@
+package SetInterface;
+
+import java.util.HashSet;
+
+public class hashSetExample {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> hs = new HashSet<String>();
+		hs.add("Ajay");
+		hs.add("Swati");
+		System.out.println(hs);
+	}
+
+}
